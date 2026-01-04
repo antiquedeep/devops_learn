1b0a6b9253472606be2a0421b6c21180f979e3f3
antiquedeep <deepakarya360@gmail.com>
2026-01-04 21:53:23 +0530

    python learning

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,27 @@
-#hi
+# # Single line comment to show and print basic hello command
+# print("Hello, Deepak")
+
+#####################################################
+
+# """
+# This is a multiline comment
+# Below we will learn about indentation in python
+# """
+# print("This has no indentation")
+    # print("this have a indentation") # this is a error line due to indentation
+
+#####################################################
+
+# # Now we will check inputs and output
+# name = input("Enter your name: ") # here name is string
+# print("Name is:", name) # Directly call in print statement
+
+# now we will print variable
+s = "Deepu"
+print(s)
+
+s = "Vidia"
+age = "29"
+city = "Delhi"
+
+print(s, age, city)
